273c86e5f0c880da33d344bbff68bb1b4478e8e5
GaelQuesadilla <gaeldev032@gmail.com>
2020-10-02 22:57:20 -0600

    Actualizacion de mensajes añadida

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pymysql as sql
 import time
 
+channel_info = ("Error", "Este es un mensaje de error" "", "")
+
 
 class DataBase:
     def __init__(self, db):
@@ -14,23 +16,28 @@ class DataBase:
         )
         self.cursor = self.conection.cursor()
 
-    def GetChannel(self, channel_search):
+    def OpenConection(self):
         self.conection.connect()
         self.cursor = self.conection.cursor()
-        self.cursor.execute(f"SELECT * FROM channels WHERE c_name = '{channel_search}';")
-        channel_info = self.cursor.fetchall()
-        self.cursor.close()
-        self.conection.close()
-        return str(channel_info)
 
-    def GetAllMessages(self, name_channel):
-        self.conection.connect()
-        self.cursor = self.conection.cursor()
-        self.cursor.execute(f"SELECT * FROM {name_channel} ORDER BY message_id DESC LIMIT 25;")
-        all_messages = self.cursor.fetchall()
+    def CloseConnection(self):
         self.cursor.close()
         self.conection.close()
+
+    def GetChannel(self, channel_search):
+        self.cursor.execute(f"SELECT * FROM channels WHERE c_name = '{channel_search}';")
+        channel_data = self.cursor.fetchall()
+        return str(channel_data)
+
+    def GetMessages(self, name_channel, message_limit):
+        self.cursor.execute(f"SELECT * FROM {name_channel} ORDER BY message_id DESC LIMIT {message_limit};")
+        all_messages = self.cursor.fetchall()
         return all_messages
+
+    def LastMessage(self, name_channel):
+        self.cursor.execute(f"Select message_id FROM {name_channel} ORDER BY message_id DESC LIMIT 1;")
+        last_message = self.cursor.fetchone()
+        return last_message
 
 
 database = DataBase("bgdw8pqbnrolcqwdgor0")
@@ -47,7 +54,6 @@ while not connect:
 
     except Exception as e:
         print("Ha ocurrido un error")
-        channel_info = ("", "")
         print(e)
 
     if channel_info[1] == "":
@@ -62,12 +68,29 @@ while not connect:
                 password_correct = True
 
 print("Has accedido :D")
-message_a = database.GetAllMessages(channel_info[3])
+message_a = database.GetMessages(channel_info[3], 25)
 for m in range(0, len(message_a)):
     message_p = message_a[-m - 1]
     print(f"{message_p[0]}|| {message_p[1]} -- {message_p[2]}")
 
+database.CloseConnection()
+
+message_l = int(message_a[0][0])
 
 while True:
+
     time.sleep(7)
 
+    database.OpenConection()
+    message_l2 = int(database.LastMessage(channel_info[3])[0])
+    if message_l != message_l2:
+        limit = message_l2 - message_l
+        message_s = database.GetMessages(channel_info[3], limit)
+
+        for m in range(0, len(message_s)):
+            message_p = message_s[-m - 1]
+            print(f"{message_p[0]}|| {message_p[1]} -- {message_p[2]}")
+
+        message_l = message_l2
+
+    database.CloseConnection()
